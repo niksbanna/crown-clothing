25eb3af9c483c49652f681ef38d86c50167794f4
Niks Banna <bannanicky0@gmail.com>
2023-01-24 18:35:59 +0530

    added and styled homepage component

---
+++ src/Pages/Homepage/Homepage.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import DirectoryMenu from '../../Components/Directory-menu/DirectoryMenu';
+import './Homepage.scss';
+
+export default function Homepage() {
+    return (
+        <div className="homepage">
+            <DirectoryMenu />
+        </div>
+    )
+}
