22f763c71fe640e3af9883d14b3eaca8821b2f48
Niks Banna <bannanicky0@gmail.com>
2023-03-09 09:35:01 +0530

    created cartItem component and added cartDropdown with cartReducer

--- src/Components/CartDropdown/CartDropdown.js
+++ src/Components/CartDropdown/CartDropdown.js
@@ -1,13 +1,25 @@
 import React from 'react';
+import { connect } from 'react-redux';
+import CartItem from '../CartItem/CartItem';
 import CustonButton from '../Custom-button/CustonButton';
 import './CartDropdown.scss';
 
-export default function CartDropdown() {
+const CartDropdown = ({ cartItems }) => {
     return (
         <div className='cart-dropdown'>
             <div className="cart-items">
-                <CustonButton>GO TO CHECKOUT</CustonButton>
+                {
+                    cartItems.map((cartItem) => (
+                        <CartItem key={cartItem.id} item={cartItem} />
+                    ))
+                }
             </div>
+            <CustonButton>GO TO CHECKOUT</CustonButton>
         </div>
     )
 }
+const mapStateToProps = ({ cart: { cartItems } }) => ({
+    cartItems
+})
+
+export default connect(mapStateToProps)(CartDropdown);
